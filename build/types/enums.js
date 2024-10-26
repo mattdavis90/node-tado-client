f984366a64a5ef2e6589e25f12b318aab44767b5
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2024-10-26 11:14:12 +0200

    chore: remove build output and git ignore it

--- build/types/enums.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DataPointType = void 0;
-var DataPointType;
-(function (DataPointType) {
-  DataPointType["PERCENTAGE"] = "PERCENTAGE";
-  DataPointType["TEMPERATURE"] = "TEMPERATURE";
-})(DataPointType || (exports.DataPointType = DataPointType = {}));
-//# sourceMappingURL=enums.js.map
